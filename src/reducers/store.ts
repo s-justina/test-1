d4d0782a78fc3271294c629ea2282af4645da39e
Justyna <jus.sobcz@gmail.com>
2021-06-21 23:52:12 +0200

    add .env; remove setTimeout from fetchData; replace history.push with Link component - WIP

--- src/reducers/store.ts
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import logger from "redux-logger";
-import { rootReducer } from "./root.reducer";
-
-export const store = createStore(rootReducer, applyMiddleware(logger));
